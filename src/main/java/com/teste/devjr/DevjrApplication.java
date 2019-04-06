65da42efa268e7bf8f0eda024672e29d35d064e2
Tainan Rodrigues <tainansilvasci@gmail.com>
2019-04-06 16:06:10 -0300

    classes creation

--- src/main/java/com/teste/devjr/DevjrApplication.java
+++ src/main/java/com/teste/devjr/DevjrApplication.java
@@ -5,21 +5,17 @@ import org.apache.commons.net.ftp.FTPClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.io.IOException;
 
 @SpringBootApplication
+@EnableScheduling
 public class DevjrApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DevjrApplication.class, args);
-
-
-        FTPUtil ftpUtil = new FTPUtil();
-
-        ftpUtil.download();
-
     }
 
 }
