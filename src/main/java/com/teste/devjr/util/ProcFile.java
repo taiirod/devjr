4b08a21b20e1a56e0cb4d51b9d0e827d4b970cfa
Tainan Rodrigues <tainansilvasci@gmail.com>
2019-04-07 12:21:51 -0300

    conteudo de .txt parseado para json

--- src/main/java/com/teste/devjr/util/ProcFile.java
+++ src/main/java/com/teste/devjr/util/ProcFile.java
@@ -1,7 +1,10 @@
 package com.teste.devjr.util;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.teste.devjr.controller.OrderItemController;
 import com.teste.devjr.model.OrderItem;
+import jdk.nashorn.internal.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +19,7 @@ public class ProcFile {
     private static final Logger log = LoggerFactory.getLogger(ProcFile.class);
 
     @Autowired
-    private OrderItemController orderItemController;
+    private OrderItemController orderItemCon;
 
     @Scheduled(fixedRate = 30000)
     public void process () {
@@ -29,14 +32,32 @@ public class ProcFile {
             for (File f: files) {
 
                 String orderDate = f.getName();
-                orderDate.split("_*_");
                 log.info(orderDate + " " + i++);
 
                 try {
                     BufferedReader br = new BufferedReader(new FileReader(file +"/"+f.getName()));
+                    int j = 0;
+                    int k = 0;
                     while(br.ready()) {
+
                         String linha = br.readLine();
-                        System.out.println(linha);
+                        String[] l = linha.split("\\|");
+                        String a = l[0];
+                        String b = l[1];
+                        String c = l[2];
+                        String d = l[3];
+                        String e = l[4];
+
+
+                        JsonObject order = new JsonObject();
+                        order.addProperty("nr", a);
+                        order.addProperty("sku", b);
+                        order.addProperty("qt", c);
+                        order.addProperty("vl", d);
+                        order.addProperty("status", e);
+                        System.out.println(order.toString());
+
+
                     }
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
