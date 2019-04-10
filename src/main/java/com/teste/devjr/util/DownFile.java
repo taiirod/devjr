30e153a7a6033bbec4da7f672609f73deb8c4caf
tainan.rodrigues <tainansilvasci@gmail.com>
2019-04-10 17:10:11 -0300

    saving files from ftp on db

--- src/main/java/com/teste/devjr/util/DownFile.java
+++ src/main/java/com/teste/devjr/util/DownFile.java
@@ -15,57 +15,66 @@ public class DownFile {
 
 
 	private static final Logger log = LoggerFactory.getLogger(DownFile.class);
-	
+
 	@Scheduled(fixedRate = 10000)
 	public void download (){
 		String server = "3.86.89.252";
 		int port = 21;
 		String user = "tainan";
 		String pass = "890iop";
-		
+
 		FTPClient ftpClient = new FTPClient();
-		
+
 		try {
 			// connect and login to the server
 			ftpClient.connect(server, port);
 			ftpClient.login(user, pass);
-			
+
 			// use local passive mode to pass firewall
 			ftpClient.enterLocalPassiveMode();
-			
+
 			System.out.println("Connected");
-			
+
 			String remoteDirPath = "pending";
 			String saveDirPath = "";
-			
+
 			DownFile.downloadDirectory(ftpClient, remoteDirPath, "", saveDirPath);
-			
+
 			// log out and disconnect from the server
 			ftpClient.logout();
 			ftpClient.disconnect();
-			
+
 			System.out.println("Disconnected");
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
 	}
-	
-	
+
+
 	public static boolean downloadSingleFile(FTPClient ftpClient,
 	                                         String remoteFilePath,
 	                                         String savePath) throws IOException {
 		File downloadFile = new File(savePath);
-		
+
 		File parentDir = downloadFile.getParentFile();
 		if (!parentDir.exists()) {
 			parentDir.mkdirs();
 		}
-		
+
 		OutputStream outputStream = new BufferedOutputStream(
 				new FileOutputStream(downloadFile));
 		try {
-			ftpClient.setFileType(FTP.ASCII_FILE_TYPE);
-			return ftpClient.retrieveFile(remoteFilePath, outputStream);
+            File file = new File("pending");
+            File[] files = file.listFiles();
+
+		    String down = downloadFile.getName();
+		    for (File f : files){
+                if (!down.equals(f.getName())){
+                    ftpClient.setFileType(FTP.ASCII_FILE_TYPE);
+
+                }
+            }
+            return ftpClient.retrieveFile(remoteFilePath, outputStream);
 		} catch (IOException ex) {
 			throw ex;
 		} finally {
@@ -79,14 +88,17 @@ public class DownFile {
 	                                     String parentDir,
 	                                     String currentDir,
 	                                     String saveDir) throws IOException {
-		
+
 		String dirToList = parentDir;
 		if (!currentDir.equals("")) {
 			dirToList += "/" + currentDir;
 		}
-		
+
 		FTPFile[] subFiles = ftpClient.listFiles(dirToList);
-		
+
+		File file = new File("pending");
+		File[] files = file.listFiles();
+
 		if (subFiles != null && subFiles.length > 0) {
 			for (FTPFile aFile : subFiles) {
 				String currentFileName = aFile.getName();
@@ -99,14 +111,14 @@ public class DownFile {
 				if (currentDir.equals("")) {
 					filePath = parentDir + "/" + currentFileName;
 				}
-				
+
 				String newDirPath = saveDir + parentDir + File.separator
 						+ currentDir + File.separator + currentFileName;
 				if (currentDir.equals("")) {
 					newDirPath = saveDir + parentDir + File.separator
 							+ currentFileName;
 				}
-				
+
 				if (aFile.isDirectory()) {
 					// create the directory in saveDir
 					File newDir = new File(newDirPath);
@@ -116,7 +128,7 @@ public class DownFile {
 					} else {
 						System.out.println("COULD NOT create the directory: " + newDirPath);
 					}
-					
+
 					// download the sub directory
 					downloadDirectory(ftpClient, dirToList, currentFileName,
 							saveDir);
@@ -133,6 +145,6 @@ public class DownFile {
 				}
 			}
 		}
-	
+
 	}
 }
